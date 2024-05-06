a111f111d56f46f75452266bfae1497d7455fc3d
Данила <@DESKTOP-VIH34LJ>
2024-05-06 22:24:56 +0300

    Добавьте файлы проекта.

---
+++ Data/DBObjects.cs
@@ -0,0 +1,49 @@
+﻿using GameShop.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameShop.Data
+{
+    public class DBObjects
+    {
+        public static async Task Initialize(AppDBContent content) 
+        {
+            await content.Database.MigrateAsync();
+
+            if (!await content.DbCategory.AnyAsync())
+            {
+                content.DbCategory.AddRange(GetCategories()); // if DbCategory is empty, initialize list of categories automatically
+                
+                await content.SaveChangesAsync();
+            }
+
+            if (!await content.DbGame.AnyAsync()) // if DbGame is empty, initialize list of items automatically
+            {
+                content.AddRange(
+                    new Game { Image = "/img/css.jpg", Name = "Counter-Strike: Source", Desc = "Неустаревающий командный тактический шутер", Details = "", TechReq = "", IsAvailable = true, Quantity = 15, Price = 300, CategoryId = 1 },
+                    new Game { Image = "/img/cof.jpg", Name = "Cry of Fear", Desc = "Известный представитель жанра survival horror", Details = "", TechReq = "", IsAvailable = true, Quantity = 20, Price = 100, CategoryId = 2 },
+                    new Game { Image = "/img/ylad.jpg", Name = "Yakuza: Like a Dragon", Desc = "Восьмой выпуск легендарной серии в стиле JRPG", Details = "", TechReq = "", IsAvailable = true, Quantity = 10, Price = 800, CategoryId = 3 },
+                    new Game { Image = "/img/va11halla.jpg", Name = "Va-11 Hall-a", Desc = "Симулятор бармена эпохи киберпанка", Details = "", TechReq = "", IsAvailable = false, Quantity = 0, Price = 200, CategoryId = 4 },
+                    new Game { Image = "/img/os.jpg", Name = "One Shot", Desc = "Сюрреалистическая приключенческая игра", Details = "", TechReq = "", IsAvailable = true, Quantity = 7, Price = 50, CategoryId = 5 }) ;
+                
+                await content.SaveChangesAsync();
+            }
+        }
+
+        private static IEnumerable<Category> GetCategories()
+        {
+            var list = new List<Category>()
+            {
+                new Category { CategoryName = "Action", CategoryDesc = "Для желающих проходить один и тот же момент тысячу раз" },
+                new Category { CategoryName = "Horror", CategoryDesc = "Для любителей пощекотать нервишки" },
+                new Category { CategoryName = "JRPG", CategoryDesc = "Японские RPG" },
+                new Category { CategoryName = "RPG", CategoryDesc = "Жанр, основанный на элементах настольных ролевых игр" },
+                new Category { CategoryName = "Quest", CategoryDesc = "Искусство создания интерактивных историй" }
+            };
+
+            return list;
+        }
+    }
+}
---
+++ Migrations/20240506083856_AddQuantity.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameShop.Migrations
+{
+    public partial class AddQuantity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "DbShopCartItem");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "DbOrderDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "DbOrder",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientName",
+                table: "DbOrder",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "DbGame",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "DbGame");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "DbShopCartItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Price",
+                table: "DbOrderDetails",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "DbOrder",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClientName",
+                table: "DbOrder",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+        }
+    }
+}
